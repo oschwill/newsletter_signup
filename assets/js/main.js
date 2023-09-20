83eece0bf9a216e91007c409c9b178dc9156f124
Oliver Schwill <o.schwill@web.de>
2023-09-20 15:45:32 +0200

    first Frontend Mentor Project

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+const errorMessage = document.querySelector('.error');
+const emailInput = document.querySelector('#email');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // clear error message
+  clearErrorMessage();
+
+  if (emailInput.value === '' || !validateEmail(emailInput.value)) {
+    errorMessage.innerText = 'valid email required';
+    emailInput.style.backgroundColor = '#efbfbf';
+    emailInput.classList.add('error');
+    return;
+  }
+
+  location.href = '../../success.html';
+});
+
+/* FUNCTIONS*/
+const validateEmail = (email) => {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
+
+const clearErrorMessage = () => {
+  errorMessage.innerText = '';
+  emailInput.style.backgroundColor = 'white';
+  emailInput.classList.remove('error');
+};
